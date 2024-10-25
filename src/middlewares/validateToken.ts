6d99a56b18d62692a467bc806d824d02759f58ef
Vipin <vipin.v@bahwancybertek.com>
2024-10-25 12:05:06 +0530

    added expection handling in validate tokem

--- src/middlewares/validateToken.ts
+++ src/middlewares/validateToken.ts
@@ -4,20 +4,24 @@ import { getSessionBasedOnToken, LOGIN_PATH } from '../utils/auth';
 
 const validateToken = async (req: any, res: Response, next: NextFunction) => {
 
-    const token = req.headers['authorization']?.split(' ')[1];
-    if (!token) {
-        return res.status(401).json('Unauthorized, token missing');
-    }
-    const session = await getSessionBasedOnToken(token);
+    try {
+        const token = req.headers['authorization']?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json('Unauthorized, token missing');
+        }
+        const session = await getSessionBasedOnToken(token);
 
-    if (!session) {
-        return res.status(401).json('Invalid session');
-    }
-    if (session && session.expiresAt < new Date()) {
+        if (!session) {
+            return res.status(401).json('Invalid session');
+        }
+        if (session && session.expiresAt < new Date()) {
+            return res.status(401).json('Session expired');
+        }
+        req.user = session?.userId;
+        next();
+    } catch (error) {
         return res.status(401).json('Session expired');
     }
-    req.user = session?.userId;
-    next();
 }
 
 export {
