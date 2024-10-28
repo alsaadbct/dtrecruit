9d9c9a85f498b4da67088455ade872ddadfb7b3a
Vipin <vipin.v@bahwancybertek.com>
2024-10-28 12:56:05 +0530

    delete mock apis

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import apiRouter from './api';
 import { BASE_URL_PREFIX } from './utils/constants';
-import { mockAPI } from './mockAPI/request';
+
 
 const app = express();
 
@@ -13,6 +13,6 @@ app.use(BASE_URL_PREFIX, apiRouter)
 
 
 
-mockAPI("user/createUsers")
+
 
 export default app;
